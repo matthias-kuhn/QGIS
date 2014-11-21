9e258760b1b3cb27c5b15a4b2af10c38005d7675
Theuns Heydenrych <theunsheydenrych@gmail.com>
2014-11-21 10:01:40 +0200

    Changes for getting QGIS build with MinGW.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -723,7 +723,7 @@ int main( int argc, char *argv[] )
 
         if ( systemEnvVars.contains( envVarName ) && envVarApply == "unset" )
         {
-#ifdef Q_WS_WIN
+#if defined(Q_WS_WIN32) || defined(WIN32)
           putenv( envVarName.toUtf8().constData() );
 #else
           unsetenv( envVarName.toUtf8().constData() );
@@ -731,7 +731,7 @@ int main( int argc, char *argv[] )
         }
         else
         {
-#ifdef Q_WS_WIN
+#if defined(Q_WS_WIN32) || defined(WIN32)
           if ( envVarApply != "undefined" || !getenv( envVarName.toUtf8().constData() ) )
             putenv( QString( "%1=%2" ).arg( envVarName ).arg( envVarValue ).toUtf8().constData() );
 #else
