b0668ca572ee75180eb0e163515ad4b58e35c89f
Matthias Kuhn <matthias@opengis.ch>
2015-09-20 22:04:22 +0200

    No postgis on docker

--- ci/travis/linux/before_script.sh
+++ ci/travis/linux/before_script.sh
@@ -1,5 +1,5 @@
-printf "[qgis_test]\nhost=localhost\ndbname=qgis_test\nuser=postgres" > ~/.pg_service.conf
-psql -c 'CREATE DATABASE qgis_test;' -U postgres
-psql -c 'CREATE EXTENSION postgis;' -U postgres -d qgis_test
-psql -f $TRAVIS_BUILD_DIR/tests/testdata/provider/testdata.sql -U postgres -d qgis_test
-
+if [ ${QT} == 5 ]; then
+  ./ci/travis/linux/qt5/before_script.sh
+else
+  ./ci/travis/linux/qt4/before_script.sh
+fi
