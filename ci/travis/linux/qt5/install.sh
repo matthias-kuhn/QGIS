56b817017a659148a09d68ac12858f5cf6674cc0
Matthias Kuhn <matthias@opengis.ch>
2015-10-07 09:21:57 +0200

    Revert "Use ccache for docker build"
    This reverts commit 878f36b10c8c358cd03c15b058c9164c56ce0743.

--- ci/travis/linux/qt5/install.sh
+++ ci/travis/linux/qt5/install.sh
@@ -16,7 +16,5 @@ docker run \
         -DENABLE_QT5=ON \
         -DWITH_DESTKOP=OFF \
         -DWITH_QTWEBKIT=OFF \
-        -DCMAKE_CXX_COMPILER:FILEPATH=/usr/lib/ccache/c++ \
-        -DCMAKE_C_COMPILER:FILEPATH=/usr/lib/ccache/cc \
         ../QGIS
 docker commit qgis-configured qgis-configured
