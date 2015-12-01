c4a739929049f98ef24e2a4370ffca7a12e53c28
Matthias Kuhn <matthias@opengis.ch>
2015-12-01 10:04:20 +0100

    [FEATURE] Add N:M relation editing possibilities
    This adds the possibility to manage data on a normalized relational
    database in N:M relations. On the relation editor in a form, the tools
    to add, delete, link and unlink work (also) on the linking table if a
    relation is visualized as N:M relation.
    
    Configuration is done through the fields tab where on the relation a
    second relation can be chosen (if there is a suitable relation in terms
    of a second relation on the linking table).
    
    Limitations
    ===========
    
    QGIS is not a database management system.
    
    It is based on assumptions about the underlying database system. In
    particular it expects
    
     * A `ON DELETE CASCADE` or similar measure on the second relation
     * Does not take care of setting the primary key when adding features.
       Either users need to be instructed to set them manually or - if it's a
       database derived value - the layers need to be in transaction mode
       (currently only activatable through the API)

---
+++ tests/testdata/provider/reltests.sql
@@ -0,0 +1,31 @@
+﻿DROP TABLE "qgis_test"."child";
+DROP TABLE "qgis_test"."parent";
+DROP SEQUENCE qgis_test.parent_id_seq;
+DROP SEQUENCE qgis_test.child_id_seq;
+
+CREATE SEQUENCE qgis_test.parent_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+
+CREATE SEQUENCE qgis_test.child_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+CREATE TABLE "qgis_test"."parent" (
+    "pk" integer PRIMARY KEY DEFAULT nextval('qgis_test.parent_id_seq'),
+    "name" CHARACTER VARYING( 255 ) COLLATE "pg_catalog"."default" UNIQUE
+);
+
+CREATE TABLE "qgis_test"."child" (
+    "pk" integer PRIMARY KEY DEFAULT nextval('qgis_test.child_id_seq'),
+    "name" CHARACTER VARYING( 255 ) COLLATE "pg_catalog"."default" UNIQUE,
+    "fk" integer,
+    FOREIGN KEY (fk) REFERENCES qgis_test.parent(pk)
+);
