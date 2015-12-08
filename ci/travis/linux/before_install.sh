9e1883b321fb3a03e7ecbae4ab993f0b3136a272
Matthias Kuhn <matthias@opengis.ch>
2015-12-08 15:09:43 +0100

    Use cache

--- ci/travis/linux/before_install.sh
+++ ci/travis/linux/before_install.sh
@@ -3,14 +3,17 @@ export DEBIAN_FRONTEND=noninteractive
 cmake --version
 clang --version
 
-mkdir -p $HOME/deps
-mkdir -p $HOME/src
+if [ -d $HOME/deps ]
+then
+  mkdir -p $HOME/deps
+  mkdir -p $HOME/src
 
-pushd $HOME/src
-wget http://download.osgeo.org/gdal/2.0.1/gdal-2.0.1.tar.gz
-tar xvf gdal-2.0.1.tar.gz
-cd gdal-2.0.1
-./configure --prefix=$HOME/deps --with-python
-make -j2
-make install
-popd
+  pushd $HOME/src
+  wget http://download.osgeo.org/gdal/2.0.1/gdal-2.0.1.tar.gz
+  tar xvf gdal-2.0.1.tar.gz
+  cd gdal-2.0.1
+  ./configure --prefix=$HOME/deps --with-python
+  make -j2
+  make install
+  popd
+fi
