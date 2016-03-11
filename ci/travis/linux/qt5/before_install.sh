2793a726992586f38d9bc2b5ab9a6cda54333b00
Matthias Kuhn <matthias@opengis.ch>
2016-03-11 18:30:58 +0100

    ii

--- ci/travis/linux/qt5/before_install.sh
+++ ci/travis/linux/qt5/before_install.sh
@@ -6,7 +6,7 @@ sudo add-apt-repository 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty m
 sudo add-apt-repository ppa:ubuntugis/ppa -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
-sudo apt-get install --force-yes --no-install-recommends --no-install-suggests \
+sudo apt-get install -y --no-install-recommends --no-install-suggests \
         bison \
         cmake \
         cmake-data \
@@ -49,7 +49,7 @@ sudo -H pip install autopep8 # TODO when switching to trusty or above: replace p
 sudo -H pip install nose2 pyyaml mock
 
 #update clang
-sudo apt-get install --force-yes llvm-3.8 llvm-3.8-dev clang-3.8 libstdc++-4.9-dev
+sudo apt-get install -y llvm-3.8 llvm-3.8-dev clang-3.8 libstdc++-4.9-dev
 
 sudo ln -s /usr/bin/ccache /usr/lib/ccache/clang-3.8
 sudo ln -s /usr/bin/ccache /usr/lib/ccache/clang++-3.8
