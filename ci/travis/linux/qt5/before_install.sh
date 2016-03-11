8916a57aa28ec91bf57441d3e24114c600d7c512
Matthias Kuhn <matthias@opengis.ch>
2016-03-11 18:01:55 +0100

    trusty style

--- ci/travis/linux/qt5/before_install.sh
+++ ci/travis/linux/qt5/before_install.sh
@@ -1,16 +1,56 @@
 export DEBIAN_FRONTEND=noninteractive
-export CORES=2
 
-##################################################
-#
-# Get precompiled dependencies
-#
-##################################################
+wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo add-apt-repository 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty main' -y
 
-pushd ${HOME}
+sudo add-apt-repository ppa:ubuntugis/ppa -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -qq
+sudo apt-get install --force-yes --no-install-recommends --no-install-suggests \
+        bison \
+        cmake \
+        cmake-data \
+        doxygen \
+        flex \
+        gdal-bin \
+        git \
+        graphviz \
+        grass-dev \
+        libexpat1-dev \
+        libfcgi-dev \
+        libgdal1-dev \
+        libgeos-dev \
+        libgsl0-dev \
+        libpq-dev \
+        libproj-dev \
+        libqca2-dev \
+        libqca2-plugin-ossl \
+        libqscintilla2-dev \
+        libqtwebkit-dev \
+        libqwt-dev \
+        libspatialindex-dev \
+        libspatialite-dev \
+        libsqlite3-dev \
+        lighttpd \
+        pkg-config \
+        poppler-utils \
+        spawn-fcgi \
+        txt2tags \
+        xauth \
+        xfonts-100dpi \
+        xfonts-75dpi \
+        xfonts-base \
+        xfonts-scalable \
+        xvfb \
+        python-pip \
+        flip \
+        jq
 
-curl -L https://github.com/opengisch/osgeo4travis/raw/binary/osgeo4travis.tar.xz | tar -JxC /home/travis
-curl -L https://cmake.org/files/v3.5/cmake-3.5.0-Linux-x86_64.tar.gz | tar --strip-components=1 -zxC /home/travis/osgeo4travis
-popd
+sudo -H pip install autopep8 # TODO when switching to trusty or above: replace python-pip with python-autopep8
+sudo -H pip install nose2 pyyaml mock
 
-# easy_install3 --prefix=${HOME}/osgeo4travis/ pyspatialite
+#update clang
+sudo apt-get install --force-yes llvm-3.8 llvm-3.8-dev clang-3.8 libstdc++-4.9-dev
+
+sudo ln -s /usr/bin/ccache /usr/lib/ccache/clang-3.8
+sudo ln -s /usr/bin/ccache /usr/lib/ccache/clang++-3.8
