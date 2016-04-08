a64de7b256feb8d148b90087880604a25e1b4542
Matthias Kuhn <matthias@opengis.ch>
2016-04-08 19:00:08 +0200

    Add lighttpd to osgeo4travis qt5

--- ci/travis/linux/qt5/script.sh
+++ ci/travis/linux/qt5/script.sh
@@ -1,5 +1,7 @@
 export PYTHONPATH=${HOME}/osgeo4travis/lib/python3.3/site-packages/
 export CTEST_PARALLEL_LEVEL=1
+export PATH=${HOME}/osgeo4travis/bin:${HOME}/osgeo4travis/sbin:${PATH}
+export LD_LIBRARY_PATH=${HOME}/osgeo4travis/lib
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
